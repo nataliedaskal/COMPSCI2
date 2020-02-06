8d292c997f17ebabee12ab1cbee5f56770efc80e
nataliedaskal <ndaskal@fwparker.org>
2020-02-06 12:48:24 -0600

    Added imports notes

---
+++ Notes/Notes3.py
@@ -0,0 +1,16 @@
+# More on loops
+
+# Basic FOR loop
+for i in range(5, 51, 5):
+    print(i, end=" ")
+
+
+# RANGE function (alternative for comprehension)
+
+# BREAK
+
+# CONTINUE
+
+# FOR ELSE
+
+# Add me as a collaborator in Github
--- Problems/ProblemSet1.py
+++ Problems/ProblemSet1.py
@@ -9,11 +9,31 @@
 # d) Make a list of all positive numbers in my_list below.
 my_list = [-77, -78, 82, 81, -40, 2, 62, 65, 74, 48, -37, -52, 90, -84, -79, -45, 47, 60, 35, -18]
 
+# 1 a
+my_list = [x for x in range(101)]
+print(my_list)
+
+# 1 b
+my_list2 = [x for x in range(20, 41) if x % 2 == 0]
+print(my_list2)
+
+# 1 c
+my_list3 = [x ** 2 for x in range(101)]
+print(my_list3)
+
+# 1 d
+my_list = [-77, -78, 82, 81, -40, 2, 62, 65, 74, 48, -37, -52, 90, -84, -79, -45, 47, 60, 35, -18]
+my_positive = [x for x in my_list if x > 0]
+print(my_positive)
+
+
 # PROBLEM 2 (Import the number list - 3pts)
 # The Problems directory contains a file called "number_list.py"
 # import this file which contains num_list
 # Print the last 5 numbers in num_list
 
+from Problems.number_list import *
+print(num_list[-5:])
 
 # PROBLEM 3 (List functions and methods - 8pts)
 # Find and print the highest number in num_list (1pt)
@@ -22,10 +42,24 @@ my_list = [-77, -78, 82, 81, -40, 2, 62, 65, 74, 48, -37, -52, 90, -84, -79, -45
 # Remove the lowest number from num_list (2pt)
 # Create and print a new list called top_ten which contains only the 10 highest numbers in num_list(2pts)
 
+print(max(num_list))
+
+print(min(num_list))
+
+print(sum(num_list) / len(num_list))
+
+num_list.sort()
+num_list.pop(0)
+print(num_list)
+
+top_ten = num_list[-10:]
+print(top_ten)
 
 # PROBLEM 4 (4pts)
 # Find the number which appears most often in num_list?
 
+my_count = [num_list.count(x) for x in num_list]
+print(num_list[my_count.index(max(my_count))])
 
 # CHALLENGE PROBLEMS (2pts)
 # TOUGH PROBLEMS, BUT FEW POINTS
@@ -33,10 +67,19 @@ my_list = [-77, -78, 82, 81, -40, 2, 62, 65, 74, 48, -37, -52, 90, -84, -79, -45
 # Find the number of prime numbers in num_list?
 # Hint: One way is to just start removing the ones that aren't
 
+for i in range(2, 10000):
+    for num in num_list:
+        if num % i == 0:
+            num_list.remove(num)
+prime_numbers = len(num_list)
+print(prime_numbers)
+
 # Find the number of palindromes
 # Hint: This may be easier to do with strings
 
-
-
-
-/Prepcode/
+palindrome_count = 0
+for x in range(len(num_list)):
+    n = str(num_list[x])
+    if n[0] == n[3] and n[2] == n[1]:
+        palindrome_count += 1
+print(palindrome_count)
