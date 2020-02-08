e9a8ecabf4278d222ea1dd93ae35553988b2937f
nataliedaskal <ndaskal@fwparker.org>
2020-02-07 18:53:39 -0600

    Added imports notes

--- Projects/myhangman.py
+++ Projects/myhangman.py
@@ -1,37 +1,108 @@
 import random
 
+gallows = [
+    '''
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========
+    ''',
+    '''
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========
+    ''',
+    '''
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========
+    ''',
+    '''
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========
+    ''',
+    '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+          |
+          |
+    =========
+    ''',
+    '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     /    |
+          |
+    =========
+    ''',
+    '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |
+    =========
+    '''
+    ]
+
+print(gallows[4])
+
 my_list = ["CALIFORNIA", "IOWA", "MAINE", "COLORADO", "OREGON", "GEORGIA"]
-
-#1
-random.shuffle(my_list)
-my_word = my_list.pop()
-print(my_word, my_list)
-
-#2
-my_word = my_list.pop(random.randrange(len(my_list)))
-print(my_word, my_list)
-
-used_letters = ["A", "S", "T"]
-my_word = "GEORGIA"
-
-# make a list of words
-# pick a random word
+word = my_list[random.randrange(6)]
+word_letters = []
+guessed_letters = []
+wrong_guesses = 0
+your_level = 0
 done = False
+for i in range(len(word)):
+    print("__ ", end="")
+
 while not done:
-    # print the gallows
-    # print the word with blanks or letters
-    guess = input("Pick a letter")
-    # check to see if already selected
-    # check if in word (if it isn't, you lose a life)
-    # check for a win or loss
-
-
-correct = True
-for letter in my_word:
-    if letter in used_letters:
-        print(letter, end=" ")
+    print(gallows[your_level])
+    correct_guesses = 0
+    letters_remaining = len(word)
+    guess = input("\nPick a letter ")
+    if guess.lower() in guessed_letters:
+        print('You already guessed that, pick a different letter.')
+        if guess.lower() not in guessed_letters:
+            guessed_letters.append(guess)
+    if guess.lower() in word.lower():
+        if guess.lower() not in guessed_letters:
+            correct_guesses += 1
+        word_letters.append(guess.lower())
     else:
-        print("_", end=" ")
-        correct = False
-
-print(correct)
+        wrong_guesses += 1
+        your_level += 1
+    for letter in word.lower():
+        if letter in word_letters:
+            print(letter + " ", end="")
+            letters_remaining -= 1
+        else:
+            print("__", end=" ")
+    if letters_remaining == 0:
+        done = True
+        print("You won! I challenge you to play again!")
+    if wrong_guesses >= 6:
+        print("You lost. I challenge you to play again!")
+        done = True
