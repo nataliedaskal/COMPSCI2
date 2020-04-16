4d10e8a6caf021cb860ab23cac9e1063af146244
nataliedaskal <ndaskal@fwparker.org>
2020-04-15 22:23:03 -0500

    Merge branch 'master' of http://github.com/fwparkercode/P2_SP20
     Conflicts:
    	Labs/CTA Ridership/cta_rider_lab.py

--- Labs/Scraping Lab/scraping-lab.py
+++ Labs/Scraping Lab/scraping-lab.py
@@ -3,9 +3,24 @@
 # Go to your favorite follow on Twitter.  (not someone who posts explicit materials please)
 # Inspect the twitter feed in Chrome.
 # You'll notice that the tweets are stored in a ordered list <ol></ol>, and individual tweets are contained as list items <li></li>.
-# Use BeautifulSoup and requests to grab the text contents of last 5 tweetslocated on the twitter page you chose.
+# Use BeautifulSoup and requests to grab the text contents of last 5 tweets located on the twitter page you chose.
 # Print the tweets in a nicely formatted way.
 # Have fun.  Again, nothing explicit.
+
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://twitter.com/GCTigerTracker"
+
+page = requests.get(url)
+print(page)
+
+soup = BeautifulSoup(page.text, "html.parser")
+print(soup.prettify())
+
+span_class= soup.find("span class")  # look for the <title> and returns the first one
+print(span_class)
+
 
 #  print("{} {}!".format("Hello", "World"))
 
