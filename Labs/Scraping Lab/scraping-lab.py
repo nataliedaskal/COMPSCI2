cfd249bce075dcf396a243f8f8937f9ba0d802a8
nataliedaskal <ndaskal@fwparker.org>
2020-04-21 10:57:30 -0500

    Merge branch 'master' of http://github.com/fwparkercode/P2_SP20
     Conflicts:
    	Labs/CTA Ridership/cta_rider_lab.py

--- Labs/Scraping Lab/scraping-lab.py
+++ Labs/Scraping Lab/scraping-lab.py
@@ -36,3 +36,4 @@ print(span_class)
 # Sample sentence:
 # Wednesday, April 4 will be Partly Cloudy/Windy with a High of 37 degrees and a low of 25, humidity at 52%.  There is 0% chance of rain with winds out of the WNW at 22 mph.
 # if the sentence is a little different than shown, that will work; do what you can.  Don't forget about our friend string.format()
+folium.Marker(location=[41.923000, -87.638461], popup="Our School")
