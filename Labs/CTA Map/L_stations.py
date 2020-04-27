792d430a1d025ecdd00368802f0e30c36272f1b9
nataliedaskal <ndaskal@fwparker.org>
2020-04-27 09:05:52 -0500

    Merge branch 'master' of http://github.com/fwparkercode/P2_SP20
     Conflicts:
    	Labs/CTA Ridership/cta_rider_lab.py

--- Labs/CTA Map/L_stations.py
+++ Labs/CTA Map/L_stations.py
@@ -10,10 +10,32 @@
 
 # Tricky parts of this one
 ## The location is in tuple format.  If you have trouble converting it, try this:
+
+import folium
+import csv
+
+with open('CTA_-_System_Information_-_List_of__L__Stops (1).csv') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+
+cta_map = folium.Map(location=[41.8781, -87.6298], zoom_start=11, titles='Stamen Toner')
+
+print(data.pop(0))
+lat_longs = [eval(x[-1]) for x in data]
+print(lat_longs[0])
+
+for i in range(len(lat_longs)):
+    folium.Marker(location=lat_longs[i], popup='<a href="http://fwparker.org">Parker</a>').add_to(cta_map)
+
+
+
 my_string = '(41.2, -87.9)'
 my_tuple = eval(my_string)
 print(my_tuple)
 print(type(my_tuple))
+
+
+cta_map.save('train-map.html')
 
 
 # If you have extra time, try to put some html into the popup.
