0575423cf0975e0964a83fb5bbda6af20babc8d4
nataliedaskal <ndaskal@fwparker.org>
2020-04-29 00:13:42 -0500

    Merge branch 'master' of http://github.com/fwparkercode/P2_SP20
     Conflicts:
    	Labs/CTA Ridership/cta_rider_lab.py

--- Labs/CTA Map/L_stations.py
+++ Labs/CTA Map/L_stations.py
@@ -13,27 +13,48 @@
 
 import folium
 import csv
+from folium import plugins
 
 with open('CTA_-_System_Information_-_List_of__L__Stops (1).csv') as f:
     reader = csv.reader(f)
     data = list(reader)
 
-cta_map = folium.Map(location=[41.8781, -87.6298], zoom_start=11, titles='Stamen Toner')
+data.pop(0)
 
 print(data.pop(0))
 lat_longs = [eval(x[-1]) for x in data]
+names = [x[3] for x in data]
 print(lat_longs[0])
-
-for i in range(len(lat_longs)):
-    folium.Marker(location=lat_longs[i], popup='<a href="http://fwparker.org">Parker</a>').add_to(cta_map)
+colors = []
 
 
+for train in data:
+    if train[7] == 'true':
+        colors.append('red')
+    elif train[8] == 'true':
+        colors.append('blue')
+    elif train[9] == 'true':
+        colors.append('green')
+    elif train[10] == 'true':
+        colors.append('#62361B')
+    elif train[11] == 'true' or train[12] == 'true':
+        colors.append('purple')
+    elif train[13] == 'true':
+        colors.append('yellow')
+    elif train[14] == 'true':
+        colors.append('pink')
+    elif train[-2] == 'true':
+        colors.append('orange')
+    else:
+        colors.append('black')
 
-my_string = '(41.2, -87.9)'
-my_tuple = eval(my_string)
-print(my_tuple)
-print(type(my_tuple))
+cta_map = folium.Map(location=[41.8781, -87.6298], zoom_start=11, titles='Stamen Toner')
 
+for i in range(len(data)):
+    folium.Marker(location=(lat_longs[i]),
+                  popup=names[i],
+                  icon=folium.plugins.BeautifyIcon(border_color=(colors[i]), icon='train', prefix='fa')
+                  ).add_to(cta_map)
 
 cta_map.save('train-map.html')
 
